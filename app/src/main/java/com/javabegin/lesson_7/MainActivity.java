133359ad0b9bb3c2a66919226421c4a6d2fd81b9
x220 <oleg.chudnovskiy.gmail.com>
2019-09-23 16:48:01 +0300

    Initial commit

---
+++ app/src/main/java/com/javabegin/lesson_7/MainActivity.java
@@ -0,0 +1,22 @@
+package com.javabegin.lesson_7;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * Домашнее задание Lesson 7:
+ *
+ *     Добавить иконку калькулятора для проекта
+ *     Создать интерфейс калькулятора
+ *     Реализовать печать цифры при нажатии на кнопку
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
