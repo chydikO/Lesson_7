5ef37e9dfe99b1c37a0d543122b663eb42bbe7e8
x220 <oleg.chudnovskiy.gmail.com>
2019-09-24 13:13:28 +0300

    add button id to main layout

--- app/src/main/java/com/javabegin/lesson_7/MainActivity.java
+++ app/src/main/java/com/javabegin/lesson_7/MainActivity.java
@@ -3,6 +3,10 @@ package com.javabegin.lesson_7;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Домашнее задание Lesson 7:
@@ -14,9 +18,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.textViewResult) TextView textViewResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
